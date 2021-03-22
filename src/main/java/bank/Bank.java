2edf0f047544245638470a2358130d44e1201e77
Thibaut Cantet <thibaut.cantet@gmail.com>
2021-03-22 13:24:46 +0100

    deposit

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -1,5 +1,17 @@
 package bank;
 
+import java.util.HashMap;
+
 public class Bank {
 
+    private HashMap<User, Float> userBalance;
+
+    public void deposit(User user, Float depositedAmount) {
+        userBalance = new HashMap<>();
+        userBalance.putIfAbsent(user, depositedAmount);
+    }
+
+    public Float getBalance(User user) {
+        return userBalance.get(user);
+    }
 }
--- src/test/java/bank/BankUTest.java
+++ src/test/java/bank/BankUTest.java
@@ -1,7 +1,38 @@
 package bank;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BankUTest {
 
+    private Bank bank;
+
+    @BeforeEach
+    void setUp() {
+        bank = new Bank();
+    }
+
+    @Test
+    void deposit_should_update_user_balance_with_deposited_amount_equal_to_500() {
+        Float depositedAmount = 500f;
+
+        final User user = new User();
+
+        bank.deposit(user, depositedAmount);
+
+        assertThat(bank.getBalance(user)).isEqualTo(depositedAmount);
+    }
+
+    @Test
+    void deposit_should_update_user_balance_with_deposited_amount_equal_to_600() {
+        Float depositedAmount = 600f;
+
+        final User user = new User();
+
+        bank.deposit(user, depositedAmount);
+
+        assertThat(bank.getBalance(user)).isEqualTo(depositedAmount);
+    }
 }
