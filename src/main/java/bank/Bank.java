7420b8462dcaf3b17386262a5b7a0f6e42c1ea5a
Thibaut Cantet <thibaut.cantet@gmail.com>
2021-03-22 13:29:44 +0100

    rename User into AccountId

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -4,20 +4,20 @@ import java.util.HashMap;
 
 public class Bank {
 
-    private final HashMap<User, Float> userBalance;
+    private final HashMap<AccountId, Float> userBalance;
 
     public Bank() {
         userBalance = new HashMap<>();
     }
 
-    public void deposit(User user, Float depositedAmount) {
+    public void deposit(AccountId accountId, Float depositedAmount) {
         if (depositedAmount < 0) {
             throw new RuntimeException("Negative amount");
         }
-        userBalance.putIfAbsent(user, depositedAmount);
+        userBalance.putIfAbsent(accountId, depositedAmount);
     }
 
-    public Float getBalance(User user) {
-        return userBalance.get(user);
+    public Float getBalance(AccountId accountId) {
+        return userBalance.get(accountId);
     }
 }
--- src/test/java/bank/BankUTest.java
+++ src/test/java/bank/BankUTest.java
@@ -19,29 +19,29 @@ class BankUTest {
     void deposit_should_update_user_balance_with_deposited_amount_equal_to_500() {
         Float depositedAmount = 500f;
 
-        final User user = new User();
+        final AccountId accountId = new AccountId();
 
-        bank.deposit(user, depositedAmount);
+        bank.deposit(accountId, depositedAmount);
 
-        assertThat(bank.getBalance(user)).isEqualTo(depositedAmount);
+        assertThat(bank.getBalance(accountId)).isEqualTo(depositedAmount);
     }
 
     @Test
     void deposit_should_update_user_balance_with_deposited_amount_equal_to_600() {
         Float depositedAmount = 600f;
 
-        final User user = new User();
+        final AccountId accountId = new AccountId();
 
-        bank.deposit(user, depositedAmount);
+        bank.deposit(accountId, depositedAmount);
 
-        assertThat(bank.getBalance(user)).isEqualTo(depositedAmount);
+        assertThat(bank.getBalance(accountId)).isEqualTo(depositedAmount);
     }
 
     @Test
     void deposit_should_throw_exception_when_amount_is_negative() {
-        final User user = new User();
+        final AccountId accountId = new AccountId();
 
-        final Throwable throwable = catchThrowable(() -> bank.deposit(user, -600f));
+        final Throwable throwable = catchThrowable(() -> bank.deposit(accountId, -600f));
 
         assertThat(throwable).isEqualToComparingFieldByField(new RuntimeException("Negative amount"));
     }
